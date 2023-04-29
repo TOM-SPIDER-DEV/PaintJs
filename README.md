a96557850c728c9f1c127b5c34d4687a0c494163
Tomas Santa Cruz <tomasesantacruz@gmail.com>
2023-04-29 10:33:22 -0300

    Add canvas drawing, erasing, color selection, and adjust the brush size functionality. Also add README.MD

---
+++ README.md
@@ -0,0 +1,23 @@
+# Canvas Drawing App
+
+This is a canvas drawing application built using HTML5 Canvas and JavaScript. It provides users with the ability to draw, erase, select colors, and adjust the brush size.
+
+## Features
+
+- **Drawing:** Use the mouse to draw freehand on the canvas.
+- **Erasing:** Switch to eraser mode to remove drawn elements from the canvas.
+- **Color Selection:** Choose a color from the color picker to set the drawing color.
+- **Brush Size:** Adjust the brush size using the provided input range control.
+- **Responsive Canvas:** The canvas automatically adjusts its size to match the window dimensions.
+
+## Usage
+
+1. Clone the repository: git clone https://github.com/TOM-SPIDER-DEV/PaintJs.git
+
+2. Open the `index.html` file in a web browser.
+
+3. Start drawing on the canvas by clicking and dragging the mouse.
+
+4. Use the pencil and eraser buttons to switch between drawing and erasing modes.
+
+5. Adjust the brush size by moving the range slider.
--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const inputColor = document.querySelector("#color"); // Get the color input element by its ID
+  const inputSize = document.querySelector("#strong-size"); // Get the size input element by its ID
   const canvas = document.querySelector("#canvas"); // Get the canvas element by its ID
   const context = canvas.getContext("2d"); // Get the 2D context of the canvas
   canvas.width = window.innerWidth; // Set the canvas width to match the window width
   canvas.height = window.innerHeight; // Set the canvas height to match the window height
   let painting = false; // Indicates whether painting is happening or not
+  let isErasing = false; // Indicates whether erasing is happening or not
 
   function draw(event) {
     if (!painting) return; // If not currently painting, exit the function
 
-    context.lineWidth = 5; // Set the line width based on the user-specified value
-    context.strokeStyle = "000"; // Set the line color based on the user-specified value
-    context.lineCap = "round"; // Set the line style to round at the ends
-    context.lineTo(event.clientX, event.clientY); // Draw a line from the current position to the mouse event coordinates
-    context.stroke(); // Draw the line on the canvas
+    if (isErasing) {
+      // Clear a rectangular area around the mouse event coordinates
+      context.clearRect(event.clientX - 10, event.clientY - 10, 20, 20);
+
+      // Alternative approach: Clear a square area around the mouse event coordinates with user-specified size
+      // const radius = inputSize.value / 2;
+      // const posX = event.clientX - radius;
+      // const posY = event.clientY - radius;
+      // context.clearRect(posX, posY, inputSize.value, inputSize.value);
+    } else {
+      context.lineWidth = inputSize.value; // Set the line width based on the user-specified value
+      context.strokeStyle = inputColor.value; // Set the line color based on the user-specified value
+      context.lineCap = "round"; // Set the line style to round at the ends
+      context.lineTo(event.clientX, event.clientY); // Draw a line from the current position to the mouse event coordinates
+      context.stroke(); // Draw the line on the canvas
+    }
   }
 
   function startPosition(event) {
@@ -29,4 +43,16 @@ document.addEventListener("DOMContentLoaded", () => {
   canvas.addEventListener("mousedown", startPosition); // Listen for the mouse click event to start drawing
   canvas.addEventListener("mouseup", endPosition); // Listen for the mouse release event to stop drawing
   canvas.addEventListener("mousemove", draw); // Listen for the mouse move event to draw while the mouse is moving
+
+  const pencilButton = document.querySelector("#pencil-button"); // Get the pencil button element by its ID
+  pencilButton.addEventListener("click", () => {
+    isErasing = false; // Set the erasing mode to false
+    canvas.style.cursor = "crosshair"; // Change the cursor style of the canvas to crosshair (indicating drawing)
+  });
+
+  const eraserButton = document.querySelector("#eraser-button"); // Get the eraser button element by its ID
+  eraserButton.addEventListener("click", () => {
+    isErasing = true; // Set the erasing mode to true
+    canvas.style.cursor = "cell"; // Change the cursor style of the canvas to cell (indicating erasing)
+  });
 });
